20a76f488d721ac37fcc8d3859ee09838e8adac7
chronize <sort@programmer.net>
2014-08-11 22:29:04 -0400

    strip unnecessary files

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index');
 
 var app = express();
 
@@ -22,8 +21,7 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', index);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
